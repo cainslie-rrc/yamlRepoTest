d023d8b91e5707aa7d20e512ae6245abe3a7f448
Curtis Ainslie <cainslie@gmail.com>
2026-01-20 12:31:59 -0600

    New files added.

---
+++ good_hello.py
@@ -0,0 +1,16 @@
+def hello():
+    """
+    Prints a greeting.
+    """
+    print("Hello, DevOps Learners!")
+
+def bye():
+    """
+    Prints a goodbye.
+    """
+    print("Goodbye, DevOps Learners!")
+
+if __name__ == "__main__":
+    hello()
+    bye()
+    
---
+++ test_main.py
@@ -0,0 +1,8 @@
+from good_hello import hello, bye
+
+def test_hello_returns_none():
+    assert hello() is None
+
+def test_bye_returns_none():
+    assert bye() is None
+    
